4d2365cc9e0589f61bcff88a9922d587600efa0c
Johannes <johannes.haapanen@gmail.com>
2021-05-23 22:59:41 +0300

    Added menu overlay to the home page

---
+++ project/dist/js/index.js
@@ -0,0 +1,24 @@
+
+
+const menuList = document.querySelector(".menu-list");
+const sideThing = document.querySelector(".side-thing");
+const menuButton = document.querySelector(".menu-button");
+const mediaLinks = document.querySelector(".media-links");
+
+let menuOpen = false;
+
+menuButton.addEventListener("click", () => {
+    console.log("Hey");
+
+    if (!menuOpen) {
+        menuList.classList.add("open");
+        sideThing.classList.add("open");
+        mediaLinks.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuList.classList.remove("open");
+        sideThing.classList.remove("open");
+        mediaLinks.classList.remove("open");
+        menuOpen = false;
+    }
+})
